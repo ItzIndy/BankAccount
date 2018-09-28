e1913e46c087551a144ed789c5af9c707feecb72
Indy Van Canegem <indy.vancanegem@student.hogent.be>
2018-09-28 09:43:53 +0200

    Add project files.

---
+++ BankAccountGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAccountGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
