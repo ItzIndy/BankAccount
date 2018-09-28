961fac3ac0a47d98112fa6c3a7e57048d3bef470
Indy Van Canegem <indy.vancanegem@student.hogent.be>
2018-09-28 09:47:56 +0200

    Creation BankAccount

---
+++ BankAccountGIT/Models/Domain/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BankingApp.Models.Domain
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        #region Properties.
+        public const decimal WithdrawCost = 0.10M; //Hier plak je de M achter het getal, want het is een double maar je toont zo aan de compiler dat het eigelijk een decimal is.
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("No negatve balance");
+                _balance = value;
+            }
+        }
+
+        public string AccountNumber { get; set; }//automatic property
+        #endregion 
+        #region Constructors
+        public BankAccount(string accountNumber)         //Constructor 1
+
+        {
+            AccountNumber = accountNumber;
+        }
+
+        //Constructor 2
+        // Je roept de vorige constructor op om accountNumber te initialiseren.
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        }
+
+        public BankAccount()
+        {
+
+        }
+        #endregion
+        #region Methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal value)
+        {
+            Balance -= value + WithdrawCost;
+        }
+        #endregion
+
+
+    }
+}
--- BankAccountGIT/Program.cs
+++ BankAccountGIT/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿
+using BankingApp.Models.Domain;
+using System;
 
-namespace BankAccountGIT
+namespace BankingApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount anotherBA = new BankAccount()
+            {
+                AccountNumber = "123-123456789-99",
+                Balance = 200
+            };
+
+            BankAccount myBA = new BankAccount("6307835468", 50);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Deposit(200);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Withdraw(200);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            Console.WriteLine($"Accountnumber = {myBA.AccountNumber} and Balance = {myBA.Balance}");
+            Console.WriteLine($"Withdrawcost is {BankAccount.WithdrawCost}");
+            Console.ReadKey();
+
         }
     }
 }
